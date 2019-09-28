ad277062af77bebbb14dea33db7ffb203273365e
RBeltran12358 <ricardo.beltran12358@gmail.com>
2019-09-27 18:38:27 -0700

    Updated Attempt

--- lab1.py
+++ lab1.py
@@ -48,8 +48,12 @@ def bin_search(target, low, high, int_list):  # must use recursion
             return 0
         else:
             return None
-
-    if len(int_list[low:high]) > 1:
+    """ Commenting it out so I can try a different method to see if I can improve my score
+    
+    searches for target in int_list[low..high] and returns index if found
+       If target is not found returns None. If list is None, raises ValueError 
+    
+    if len(int_list[low:high]) >= 1:
         mid = 1 + int(high - 1 / 2)  # In order to get the middle value
         if int_list[mid] == target:  # Checks if the middle value matches the target
             return mid  # returns the index, not the value
@@ -63,4 +67,25 @@ def bin_search(target, low, high, int_list):  # must use recursion
     else:
         return None  # If target isn't found in the list, this makes sure to
         # return None, since the only way to get to the else statement is
-        # to not have triggered any of the other bits of code that
+        # to not have triggered any of the other bits of code that """
+
+    if high >= low:
+        mid = low + int((high - low) / 2)  # This gets the middle index as an int, not a float
+
+        # If the target is in the middle, this is to catch it right away.
+        if int_list[mid] == target:
+            return mid
+
+        # If element is smaller than mid, then it must be
+        # in the lower half of the list
+        elif int_list[mid] > target:
+            return bin_search(target, low, mid - 1, int_list)
+
+        # If not, then the target can only be in the upper half,
+        # so we adjust our checkpoints for the next call to the function.
+        else:
+            return bin_search(target, mid + 1, high, int_list)
+
+    else:
+        # If the target is not present in the array
+        return None
